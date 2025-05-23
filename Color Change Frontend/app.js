d5e108f711d069753687ca9649022ead11791d96
Amin Alashkar <585017@eriesd.org>
2025-05-23 09:40:48 -0400

    Update app.js

--- Color Change Frontend/app.js
+++ Color Change Frontend/app.js
@@ -1,17 +1,3 @@
-function color_change(color) {
-  fetch("http://10.220.1.123:8000/colors/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      hex_color: color,
-    }),
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
-}
 
 const colorDisplay = document.getElementById('colorDisplay');
 
