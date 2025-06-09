f7ee2830a179947211c895c32cdb59d2b567f297
Amin Alashkar <585017@eriesd.org>
2025-06-08 20:22:48 -0400

    Update code.py

--- Color Change Frontend/code.py
+++ Color Change Frontend/code.py
@@ -6,7 +6,7 @@ num_pixels = 10
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, auto_write=False)
 
 # الألوان المطلوبة
-low_red = (127, 0, 0)   # سطوع 50%
+low_red = (50, 0, 0)   # سطوع 50%
 high_red = (255, 0, 0)  # سطوع 100%
 
 while True:
@@ -17,9 +17,9 @@ while True:
     # ومضة قصيرة على 100%
     pixels.fill(high_red)
     pixels.show()
-    time.sleep(0.1)  # مدة الوميض (قصيرة وواضحة)
+    time.sleep(.2)  # مدة الوميض (قصيرة وواضحة)
     
     # رجعهم مباشرة للـ 50%
     pixels.fill(low_red)
     pixels.show()
-    time.sleep(0.9)  # انتظار لباقي الثانية
+    time.sleep(0.2)  # انتظار لباقي الثانية
