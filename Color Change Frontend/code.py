ff18acda4b9af4ad17afc1c2c385f3956486558f
Amin Alashkar <585017@eriesd.org>
2025-06-08 20:28:39 -0400

    Update code.py

--- Color Change Frontend/code.py
+++ Color Change Frontend/code.py
@@ -2,24 +2,43 @@ import time
 import board
 import neopixel
 
+# الإعدادات
 num_pixels = 10
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, auto_write=False)
 
-# الألوان المطلوبة
-low_red = (50, 0, 0)   # سطوع 50%
+# الألوان
+low_red = (50, 0, 0)    # سطوع 50%
 high_red = (255, 0, 0)  # سطوع 100%
+off = (0, 0, 0)
 
-while True:
-    # خليهم كلهم على سطوع 50%
-    pixels.fill(low_red)
+### 🐍 مرحلة الترحيب - حركة الأفعى ###
+for i in range(num_pixels):
+    pixels.fill(off)
+    pixels[i] = high_red
     pixels.show()
-    
-    # ومضة قصيرة على 100%
+    time.sleep(0.1)
+
+### 💥 وميضتين سريعتين ###
+for _ in range(2):
     pixels.fill(high_red)
     pixels.show()
-    time.sleep(.2)  # مدة الوميض (قصيرة وواضحة)
-    
-    # رجعهم مباشرة للـ 50%
+    time.sleep(0.1)
+    pixels.fill(off)
+    pixels.show()
+    time.sleep(0.1)
+
+### 🔁 الآن يبدأ الإنميشن الأساسي ###
+while True:
+    # ضوء ثابت بسطوع منخفض
     pixels.fill(low_red)
     pixels.show()
-    time.sleep(0.2)  # انتظار لباقي الثانية
+
+    # ومضة سريعة على السطوع العالي
+    pixels.fill(high_red)
+    pixels.show()
+    time.sleep(0.2)
+
+    # رجوع للسطوع المنخفض
+    pixels.fill(low_red)
+    pixels.show()
+    time.sleep(0.2)
