44652041f2ce464a9d15fa78980652349d9589de
Amin Alashkar <585017@eriesd.org>
2025-06-08 21:25:30 -0400

    Update code.py

--- Color Change Frontend/code.py
+++ Color Change Frontend/code.py
@@ -17,10 +17,27 @@ tail3 = (30, 30, 30)
 low_white = (150, 150, 150)
 high_white = (255, 255, 255)
 
-# سويتش
+# سويتش الشرطة
 switch = digitalio.DigitalInOut(board.D7)
 switch.direction = digitalio.Direction.INPUT
 switch.pull = digitalio.Pull.UP
+
+# زر A
+button_a = digitalio.DigitalInOut(board.BUTTON_A)
+button_a.direction = digitalio.Direction.INPUT
+button_a.pull = digitalio.Pull.UP
+
+# زر B
+button_b = digitalio.DigitalInOut(board.BUTTON_B)
+button_b.direction = digitalio.Direction.INPUT
+button_b.pull = digitalio.Pull.UP
+
+# هل الأضواء شغالة؟
+leds_on = True
+
+# تتبع آخر حالة للزر
+last_a = button_a.value
+last_b = button_b.value
 
 def welcome_snake():
     for _ in range(2):
@@ -47,7 +64,6 @@ def welcome_flash():
         time.sleep(0.1)
 
 def police_front_animation():
-    # Flash أحمر مرتين
     for _ in range(2):
         pixels.fill((255, 0, 0))
         pixels.show()
@@ -56,7 +72,6 @@ def police_front_animation():
         pixels.show()
         time.sleep(0.1)
 
-    # Flash أزرق مرتين
     for _ in range(2):
         pixels.fill((0, 0, 255))
         pixels.show()
@@ -65,27 +80,43 @@ def police_front_animation():
         pixels.show()
         time.sleep(0.1)
 
-    # Sweep
     for i in range(num_pixels):
         pixels.fill(off)
         pixels[i] = (255, 0, 0) if i % 2 == 0 else (0, 0, 255)
         pixels.show()
         time.sleep(0.05)
 
-# تنفيذ الترحيب مرة
+# تشغيل الترحيب مرة وحدة
 welcome_snake()
 welcome_flash()
 
-# نار بيضاء ديناميكية
 states = [True] * num_pixels
 
 while True:
-    if not switch.value:  # OFF = أنميشن نار
+    # تابع الضغط على الزرين (كبسة وحدة للتبديل)
+    current_a = button_a.value
+    current_b = button_b.value
+
+    if (not current_a and last_a) or (not current_b and last_b):
+        leds_on = not leds_on  # اقلب الحالة
+        if not leds_on:
+            pixels.fill(off)
+            pixels.show()
+        time.sleep(0.3)  # ديبونسينغ بسيط
+
+    last_a = current_a
+    last_b = current_b
+
+    if not leds_on:
+        continue  # لا تعمل شيء إذا الأضواء مطفية
+
+    # اشتغل حسب وضع السويتش
+    if not switch.value:
         for i in range(num_pixels):
             if random.random() < 0.1:
                 states[i] = not states[i]
             pixels[i] = high_white if states[i] else low_white
         pixels.show()
         time.sleep(0.2)
-    else:  # ON = شرطة
+    else:
         police_front_animation()
