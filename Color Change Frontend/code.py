5298234c1f9473b557cea3ec0e021fcb3286bda4
Amin Alashkar <585017@eriesd.org>
2025-06-08 21:08:03 -0400

    Update code.py

--- Color Change Frontend/code.py
+++ Color Change Frontend/code.py
@@ -1,68 +1,56 @@
 import time
 import board
 import neopixel
-import random
 
 num_pixels = 10
 pixels = neopixel.NeoPixel(board.NEOPIXEL, num_pixels, auto_write=False)
 
+# ألوان الرأس والذيل
+snake_head = (255, 0, 0)
+tail1 = (127, 0, 0)
+tail2 = (64, 0, 0)
+tail3 = (25, 0, 0)
 off = (0, 0, 0)
 
-# ألوان الأفعى
-snake_head = (255, 255, 255)
-tail1 = (180, 180, 180)
-tail2 = (100, 100, 100)
-tail3 = (30, 30, 30)
-
-# ألوان النار البيضاء
-low_white = (150, 150, 150)
-high_white = (255, 255, 255)
-
-# -----------------------
-# 💥 المرحلة 1: مشهد الترحيب مرة وحدة فقط
-# -----------------------
-
-def welcome_snake():
-    for _ in range(2):
-        for i in range(num_pixels):
-            pixels.fill(off)
-            if i < num_pixels:
-                pixels[i] = snake_head
-            if i - 1 >= 0:
-                pixels[i - 1] = tail1
-            if i - 2 >= 0:
-                pixels[i - 2] = tail2
-            if i - 3 >= 0:
-                pixels[i - 3] = tail3
-            pixels.show()
-            time.sleep(0.12)
-
-def welcome_flash():
-    for _ in range(2):
-        pixels.fill((255, 255, 255))
-        pixels.show()
-        time.sleep(0.1)
+# أنيميشن الأفعى (تمر مرتين)
+for _ in range(2):
+    for i in range(num_pixels):
         pixels.fill(off)
+        if i < num_pixels:
+            pixels[i] = snake_head
+        if i-1 >= 0:
+            pixels[i-1] = tail1
+        if i-2 >= 0:
+            pixels[i-2] = tail2
+        if i-3 >= 0:
+            pixels[i-3] = tail3
         pixels.show()
-        time.sleep(0.1)
+        time.sleep(0.13)
 
-# تنفيذها مرة وحدة بس عند بداية التشغيل
-welcome_snake()
-welcome_flash()
+### 💥 وميضتين سريعتين ###
+for _ in range(2):
+    pixels.fill(snake_head)
+    pixels.show()
+    time.sleep(0.1)
+    pixels.fill(off)
+    pixels.show()
+    time.sleep(0.1)
 
-# -----------------------
-# 🔥 المرحلة 2: أنميشن النار الأبيض - دائم لا نهائي
-# -----------------------
-
-states = [True] * num_pixels  # كل ليد تبدأ في الوضع العالي
+### 🔁 يبدأ الإنميشن الأساسي ###
+low_red = (50, 0, 0)
+high_red = (255, 0, 0)
 
 while True:
-    for i in range(num_pixels):
-        if random.random() < 0.1:
-            states[i] = not states[i]
-        
-        color = high_white if states[i] else low_white
-        pixels[i] = color
-
+    # سطوع منخفض دائم
+    pixels.fill(low_red)
     pixels.show()
-    time.sleep(0.2) 
+
+    # ومضة سريعة
+    pixels.fill(high_red)
+    pixels.show()
+    time.sleep(0.2)
+
+    # رجوع للسطوع المنخفض
+    pixels.fill(low_red)
+    pixels.show()
+    time.sleep(0.2)
