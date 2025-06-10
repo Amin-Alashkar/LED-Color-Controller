31ff103f1a0205707786388274de1b12460159a7
Amin Alashkar <585017@eriesd.org>
2025-06-09 20:27:47 -0400

    Update code.py

--- Color Change Frontend/code.py
+++ Color Change Frontend/code.py
@@ -1,3 +1,5 @@
-python3 -m venv playground-env
-source playground-env/bin/activate
-pip install adafruit-circuitplayground
+(playground-env) student@raspberrypi:/media/student/CIRCUITPY $ pip install adafruit-circuitplayground
+Looking in indexes: https://pypi.org/simple, https://www.piwheels.org/simple
+ERROR: Could not find a version that satisfies the requirement adafruit-circuitplayground (from versions: none)
+ERROR: No matching distribution found for adafruit-circuitplayground
+(playground-env) student@raspberrypi:/media/student/CIRCUITPY $ 
